0df8cf06dddea5d80bf586f905209ecf10ee37c6
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-07 15:52:35 -0300

    :rocket: add is pangram

--- codewars/6-kyu/Detect Pangram/src/main.rs
+++ codewars/6-kyu/Detect Pangram/src/main.rs
@@ -3,6 +3,7 @@
 // For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
 // Link: https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/rust
+
 fn is_pangram(s: &str) -> bool {
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
     for v in alphabet.chars() {
