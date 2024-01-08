b720783871ba3ee4abbe365db770479441eb55db
Hugo Orlando <hugo.orlando2050@gmail.com>
2024-01-08 17:59:04 -0300

    :rocket: add Sum of Pairs

---
+++ codewars/5-kyu/Sum of Pairs/src/main.rs
@@ -0,0 +1,67 @@
+// Sum of Pairs
+// Given a list of integers and a single sum value, return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+// If there are two or more pairs with the required sum, the pair whose second element has the smallest index is the solution.
+/*
+sum_pairs([11, 3, 7, 5],         10)
+#              ^--^      3 + 7 = 10
+== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],         6)
+#          ^-----^         4 + 2 = 6, indices: 0, 2 *
+#             ^-----^      3 + 3 = 6, indices: 1, 3
+#                ^-----^   2 + 4 = 6, indices: 2, 4
+#  * the correct answer is the pair whose second value has the smallest index
+== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)
+#  there are no pairs of values that can be added to produce 2.
+== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],         10)
+#              ^-----------^   5 + 5 = 10, indices: 1, 5
+#                    ^--^      3 + 7 = 10, indices: 3, 4 *
+#  * the correct answer is the pair whose second value has the smallest index
+== [3, 7]
+*/
+// Negative numbers and duplicate numbers can and will appear.
+
+// NOTE: There will also be lists tested of lengths upwards of 10,000,000 elements. Be sure your code doesn't time out.
+// Link: https://www.codewars.com/kata/54d81488b981293527000c8f/train/rust
+
+use std::collections::HashSet;
+
+fn sum_pairs(ints: &[i8], sum: i8) -> Option<(i8, i8)> {
+    let mut seen = HashSet::new();
+
+    for &num in ints {
+        let complement = sum - num;
+
+        if seen.contains(&complement) {
+            return Some((complement, num));
+        }
+
+        seen.insert(num);
+    }
+
+    None
+}
+
+#[test]
+fn returns_expected() {
+    let l1 = [1, 4, 8, 7, 3, 15];
+    let l2 = [1, -2, 3, 0, -6, 1];
+    let l3 = [20, -13, 40];
+    let l4 = [1, 2, 3, 4, 1, 0];
+    let l5 = [10, 5, 2, 3, 7, 5];
+    let l6 = [4, -2, 3, 3, 4];
+    let l7 = [0, 2, 0];
+    let l8 = [5, 9, 13, -3];
+    assert_eq!(sum_pairs(&l1, 8), Some((1, 7)));
+    assert_eq!(sum_pairs(&l2, -6), Some((0, -6)));
+    assert_eq!(sum_pairs(&l3, -7), None);
+    assert_eq!(sum_pairs(&l4, 2), Some((1, 1)));
+    assert_eq!(sum_pairs(&l5, 10), Some((3, 7)));
+    assert_eq!(sum_pairs(&l6, 8), Some((4, 4)));
+    assert_eq!(sum_pairs(&l7, 0), Some((0, 0)));
+    assert_eq!(sum_pairs(&l8, 10), Some((13, -3)));
+}
