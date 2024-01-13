ef7ca4ce188c817fc481356f22593aa88c3b8238
mnsh4 <hugo.orlando2050@gmail.com>
2024-01-13 20:09:33 -0300

    :rocket: Sum of positive

---
+++ codewars/8-kyu/Sum of positive/src/main.rs
@@ -0,0 +1,39 @@
+// Sum of positive
+
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// Link: https://www.codewars.com/kata/5715eaedb436cf5606000381/train/rust
+
+fn positive_sum(slice: &[i32]) -> i32 {
+    let mut sum = 0;
+    for v in slice.iter() {
+        if v > &0 {
+            sum += v;
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn some_examples() {
+        assert_eq!(positive_sum(&[1, 2, 3, 4, 5]), 15);
+        assert_eq!(positive_sum(&[1, -2, 3, 4, 5]), 13);
+        assert_eq!(positive_sum(&[-1, 2, 3, 4, -5]), 9);
+    }
+
+    #[test]
+    fn empty_list() {
+        assert_eq!(positive_sum(&[]), 0);
+    }
+
+    #[test]
+    fn all_negative() {
+        assert_eq!(positive_sum(&[-1, -2, -3, -4, -5]), 0);
+    }
+}
