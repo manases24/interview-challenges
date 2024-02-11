d4f2252e6e5234aec720eac6d5cf39ca2019a398
mnsh5 <hugo.orlando2050@gmail.com>
2024-02-10 22:33:16 -0300

    :rocket: add staircase solution

--- HackerRank/Algorithms/Warmup/Staircase/staircase.rb
+++ HackerRank/Algorithms/Warmup/Staircase/staircase.rb
@@ -1,3 +1,13 @@
+# Staircase detail
+# This is a staircase of size n = 4
+
+   #
+  ##
+ ###
+####
+
+# Write a program that prints a staircase of size n
+
 def staircase(n)
   (1..n).each do |i|
     spaces = " " * (n-i)
