61e036a069c4c95c91ac78bffa915b4c9106ec7e
mnsh5 <hugo.orlando2050@gmail.com>
2024-03-27 16:39:09 -0300

    add Terminal game move function

--- codewars/8-kyu/Grasshopper - Terminal game move function/main.go
+++ codewars/8-kyu/Grasshopper - Terminal game move function/main.go
@@ -12,5 +12,5 @@ package move
 // Link: https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/go
 
 func Move(position int, roll int) int {
-	return (roll * 2) + position
+	return position + (roll * 2)
 }
