537588d5b1cb728e0c9d9b6ae875e9a118392a08
mnsh5 <hugo.orlando2050@gmail.com>
2024-07-04 15:08:08 +0000

    doc updated

--- leetcode/01-Two-Sum/src/main.ts
+++ leetcode/01-Two-Sum/src/main.ts
@@ -1,3 +1,6 @@
+// Two Sum
+// Link: https://leetcode.com/problems/two-sum/
+
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
@@ -14,6 +17,17 @@
 // Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
+
 export function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
   return [1, 2];
 }
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([3, 2, 4], 6));
